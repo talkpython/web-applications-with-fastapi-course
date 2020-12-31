dba3a04ca6bedf8d526fc2908aaf4b55661f886e
Michael Kennedy <mikeckennedy@gmail.com>
2020-12-31 09:14:21 -0800

    Starter code for chapter 4.

---
+++ code/ch4-templates/main.py
@@ -0,0 +1,15 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def index():
+    return {
+        'message': "Hello world"
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
