857961a12d0e1e93d266bb5404998ecb0fad0696
Michael Kennedy <mikeckennedy@gmail.com>
2020-12-31 11:51:29 -0800

    Real home page UI and overall design.

--- code/ch4-templates/main.py
+++ code/ch4-templates/main.py
@@ -12,7 +12,7 @@ app = fastapi.FastAPI()
 
 def main():
     configure()
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
 
 
 def configure():
--- code/ch4-templates/views/home.py
+++ code/ch4-templates/views/home.py
@@ -6,9 +6,16 @@ router = fastapi.APIRouter()
 
 @router.get('/')
 @template()
-def index(user: str = 'anon'):
+def index():
     return {
-        'user_name': user
+        'package_count': 274_000,
+        'release_count': 2_234_847,
+        'user_count': 73_874,
+        'packages': [
+            {'id': 'fastapi', 'summary': "A great web framework"},
+            {'id': 'uvicorn', 'summary': "Your favorite ASGI server"},
+            {'id': 'httpx', 'summary': "Requests for an async world"},
+        ]
     }
 
 
