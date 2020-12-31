e530fa0e2739ec73d5fcdd6879c54e05cd2ae31d
Michael Kennedy <mikeckennedy@gmail.com>
2020-12-31 08:56:39 -0800

    Basic fastapi web app in place.

--- code/ch3-hello-fastapi-world/main.py
+++ code/ch3-hello-fastapi-world/main.py
@@ -1,1 +1,14 @@
-print("Hello web world!")
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def index():
+    return {
+        'message': "Hello world"
+    }
+
+
+uvicorn.run(app)
