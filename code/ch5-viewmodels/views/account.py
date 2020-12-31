a31e816a3d7ddd16d66f284e01b2f7c177aab12e
Michael Kennedy <mikeckennedy@gmail.com>
2020-12-31 11:55:34 -0800

    Starter code for chapter 5, view models.

---
+++ code/ch5-viewmodels/views/account.py
@@ -0,0 +1,24 @@
+import fastapi
+from fastapi_chameleon import template
+
+router = fastapi.APIRouter()
+
+
+@router.get('/account')
+def index():
+    return {}
+
+
+@router.get('/account/register')
+def register():
+    return {}
+
+
+@router.get('/account/login')
+def login():
+    return {}
+
+
+@router.get('/account/logout')
+def logout():
+    return {}
