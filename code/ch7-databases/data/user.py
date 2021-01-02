50a1e30aebff33ce2818a53c7d42ee37a7cefbf2
Michael Kennedy <mikeckennedy@gmail.com>
2021-01-01 21:55:48 -0800

    User class and corresponding user table created.

--- code/ch7-databases/data/user.py
+++ code/ch7-databases/data/user.py
@@ -1,13 +1,17 @@
 import datetime
+import sqlalchemy as sa
+
+from data.modelbase import SqlAlchemyBase
 
 
-class User:
+class User(SqlAlchemyBase):
 
-    def __init__(self, name, email, hashed_password):
-        self.id = 1
-        self.name = name
-        self.email = email
-        self.hash_password = hashed_password
-        self.created_date = None
-        self.profile_image_url = ""
-        self.last_login: datetime.datetime = None
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String)
+    email = sa.Column(sa.String, index=True, unique=True)
+    hash_password = sa.Column(sa.String)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    last_login = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    profile_image_url = sa.Column(sa.String)
