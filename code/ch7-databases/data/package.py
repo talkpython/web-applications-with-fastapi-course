c94bea3db4777999ab0b5ff44cde8fce99ec2b4e
Michael Kennedy <mikeckennedy@gmail.com>
2021-01-01 22:05:00 -0800

    Package and release classes and corresponding tables created.

--- code/ch7-databases/data/package.py
+++ code/ch7-databases/data/package.py
@@ -1,21 +1,44 @@
-class Package:
+import datetime
+from typing import List
 
-    def __init__(self,
-                 package_name: str,
-                 summary: str,
-                 description: str,
-                 home_page: str,
-                 lic: str,
-                 author_name: str,
-                 maintainers: list = None,
-                 ):
-        if maintainers is None:
-            maintainers = []
-        self.maintainers = maintainers
-        self.author_name = author_name
-        self.license = lic
-        self.home_page = home_page
-        self.description = description
-        self.summary = summary
-        self.package_name = package_name
-        self.id = package_name
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from data.modelbase import SqlAlchemyBase
+from data.release import Release
+
+
+class Package(SqlAlchemyBase):
+    __tablename__ = 'packages'
+
+    id: str = sa.Column(sa.String, primary_key=True)
+    created_date: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    last_updated: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    summary: str = sa.Column(sa.String, nullable=False)
+    description: str = sa.Column(sa.String, nullable=True)
+
+    home_page: str = sa.Column(sa.String)
+    docs_url: str = sa.Column(sa.String)
+    package_url: str = sa.Column(sa.String)
+
+    author_name: str = sa.Column(sa.String)
+    author_email: str = sa.Column(sa.String, index=True)
+
+    license: str = sa.Column(sa.String, index=True)
+
+    # releases relationship
+    releases: List[Release] = orm.relation("Release", order_by=[
+        Release.major_ver.desc(),
+        Release.minor_ver.desc(),
+        Release.build_ver.desc(),
+    ], back_populates='package')
+
+    def __repr__(self):
+        return '<Package {}>'.format(self.id)
+
+
+# p = Package()  # one query
+#
+# print(p.id)
+# print("All releases")
+# for r in p.releases:
+#     print("{}.{}.{}".format(r.major_ver, r.minor_ver, r.build_ver))
--- code/ch7-databases/data/user.py
+++ code/ch7-databases/data/user.py
@@ -5,13 +5,13 @@ from data.modelbase import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase):
-
     __tablename__ = 'users'
 
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    name = sa.Column(sa.String)
-    email = sa.Column(sa.String, index=True, unique=True)
-    hash_password = sa.Column(sa.String)
-    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
-    last_login = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
-    profile_image_url = sa.Column(sa.String)
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name: str = sa.Column(sa.String)
+    email: str = sa.Column(sa.String, index=True, unique=True)
+    hash_password: str = sa.Column(sa.String)
+    created_date: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    last_login: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    profile_image_url: str = sa.Column(sa.String)
+
