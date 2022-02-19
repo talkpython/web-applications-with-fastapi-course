9ad86650dc34c1fd211a02b7b4ddd12f6ab1b00e
Andrei Horbach <andrewhorbach@gmail.com>
2022-02-19 17:56:54 +0300

    chore: render basic html template

--- webapps_fa/main.py
+++ webapps_fa/main.py
@@ -1,15 +1,26 @@
+from pathlib import Path
+
 import fastapi
 import uvicorn
 
+import fastapi_chameleon
+from fastapi_chameleon import template
+
 app = fastapi.FastAPI()
+
+dev_mode = True
+
+BASE_DIR = Path(__file__).resolve().parent
+template_folder = str(BASE_DIR / 'templates')
+fastapi_chameleon.global_init(template_folder, auto_reload=dev_mode)
 
 
 @app.get('/')
-def index():
-    content = """
-    <h1>Hello FastApi</h1>
-    """
-    return fastapi.responses.HTMLResponse(content)
+@template(template_file='index.html')
+def index(user: str = 'unknown'):
+    return {
+        'user_name': user,
+    }
 
 
 if __name__ == '__main__':
